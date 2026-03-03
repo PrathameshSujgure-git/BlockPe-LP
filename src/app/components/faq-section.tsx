f2dbf62e73cf6e299f346ad3410b3724b5b6abb8
sethihq <rajatsethi.sethi7@gmail.com>
2026-03-03 18:13:13 +0530

    fixes/mobile faq section

--- src/app/components/faq-section.tsx
+++ src/app/components/faq-section.tsx
@@ -205,7 +205,7 @@ export function FAQSection() {
   const { playToggle } = useAccordionSound();
 
   return (
-    <section className="py-[120px]">
+    <section className="py-[80px] lg:py-[120px]">
       <div className="max-w-[390px] lg:max-w-[1200px] mx-auto px-4 lg:px-[52px]">
         <div className="flex flex-col lg:flex-row items-start justify-between">
           {/* Left */}
